53ae43c4df1e4171e6fd6c56553b097e6c9d6035
JungHyoungHo <hhjung1202@naver.com>
2018-07-19 20:59:47 +0900

    20180719 edit

--- 20180719/ResDenseNet.py
+++ 20180719/ResDenseNet.py
@@ -67,19 +67,48 @@ class _DenseLayer(nn.Sequential):
         self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
                         kernel_size=3, stride=1, padding=1, bias=False)),
         self.drop_rate = drop_rate
+        self.growth_rate = growth_rate
+
+        # self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
 
         new_features = super(_DenseLayer, self).forward(x)
+        new_features = new_features + x[:, self.growth_rate * -1:, :, :]
+        # new_features = self.relu(new_features)
+        if self.drop_rate > 0:
+            new_features = F.dropout(new_features, p=self.drop_rate, training=self.training)
+        return torch.cat([x, new_features], 1)
+
+class _DenseLayer_init(nn.Sequential):
+    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate):
+        super(_DenseLayer_init, self).__init__()
+        self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
+        self.add_module('relu1', nn.ReLU(inplace=True)),
+        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size *
+                        growth_rate, kernel_size=1, stride=1, bias=False)),
+        self.add_module('norm2', nn.BatchNorm2d(bn_size * growth_rate)),
+        self.add_module('relu2', nn.ReLU(inplace=True)),
+        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
+                        kernel_size=3, stride=1, padding=1, bias=False)),
+        self.drop_rate = drop_rate
+
+    def forward(self, x):
+        new_features = super(_DenseLayer_init, self).forward(x)
+
         if self.drop_rate > 0:
             new_features = F.dropout(new_features, p=self.drop_rate, training=self.training)
         return torch.cat([x, new_features], 1)
 
 
+
 class _DenseBlock(nn.Sequential):
     def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate):
         super(_DenseBlock, self).__init__()
-        for i in range(num_layers):
+        
+        layer = _DenseLayer_init(num_input_features, growth_rate, bn_size, drop_rate)
+        self.add_module('denselayer1', layer)
+        for i in range(1, num_layers):
             layer = _DenseLayer(num_input_features + i * growth_rate, growth_rate, bn_size, drop_rate)
             self.add_module('denselayer%d' % (i + 1), layer)
 
@@ -96,7 +125,7 @@ class _Transition(nn.Sequential):
 
 class DenseNet(nn.Module):
 
-    def __init__(self, growth_rate=12, block_config=(6,),
+    def __init__(self, growth_rate=12, block_config=(6,6),
                  num_init_features=16, bn_size=4, drop_rate=0, num_classes=10):
 
         super(DenseNet, self).__init__()
@@ -125,7 +154,7 @@ class DenseNet(nn.Module):
         self.features.add_module('norm5', nn.BatchNorm2d(num_features))
 
         # Linear layer
-        self.classifier = nn.Linear(17248, num_classes)
+        self.classifier = nn.Linear(4176, num_classes)
 
         # Official init from torch repo.
         for m in self.modules():
@@ -145,153 +174,6 @@ class DenseNet(nn.Module):
 
         return out
 
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-class ResNet(nn.Module):
-
-    def __init__(self, block, layers, num_classes=10):
-        self.inplanes = 64
-        super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1,
-                               bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(kernel_size=7, stride=1)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-
-        return x
-
-def resnet18(pretrained=False, **kwargs):
-    """Constructs a ResNet-18 model.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
-    return model
-
-
-# class Vgg(nn.Module):
-# 	def __init__(self, num_classes=10):
-# 		super(Vgg, self).__init__()
-
-# 		self.features = nn.Sequential(
-# 			nn.Conv2d(3, 64, kernel_size=3, padding=1),
-# 			nn.BatchNorm2d(64),
-# 			nn.ReLU(),
-# 			nn.Conv2d(64, 64, kernel_size=3, padding = 1),
-# 			nn.BatchNorm2d(64),
-# 			nn.ReLU(),
-
-# 			)
-# 		self.LSTM = nn.LSTM(16, 8, num_layers = 2, batch_first = True, bidirectional = True)
-# 		self.features2 = nn.Sequential (
-# 			nn.Conv2d(256, 128, kernel_size=3, padding=1),
-# 			nn.BatchNorm2d(128),
-# 			nn.ReLU(),
-# 			nn.Conv2d(128, 64, kernel_size=3, padding = 1),
-# 			nn.BatchNorm2d(64),
-# 			nn.ReLU(),
-# 			nn.MaxPool2d(kernel_size=2, stride=2),
-# 		)
-# 		self.classifier = nn.Linear(128*4*2*4, num_classes)
-
-# 	def forward(self, x):
-# 		x = self.features(x)
-# 		x = slidedImage(x)
-# 		x = x.view(x.size(0)*x.size(1)*8, 8, 16)
-# 		h = Variable(torch.zeros(4,x.size(0), 8).cuda())
-# 		c = Variable(torch.zeros(4,x.size(0), 8).cuda())
-# 		x, (h1,c1) = self.LSTM(x, (h , c))
-# 		x = x.contiguous().view(x.size(0)//64//8, x.size(1)*32, 16,16) 
-# 		x = self.features2(x)
-# 		x = x.view(x.size(0), -1)
-# 		# x.size()=[batch_size, channel, width, height]
-# 		#      [128, 512, 2, 2]
-# 		# flatten 결과 => [128, 512x2x2]
-# 		x = self.classifier(x)
-# 		x = F.dropout2d(x, p = 0.5, training = self.training)
-# 		return x
 
 
 model = DenseNet()
