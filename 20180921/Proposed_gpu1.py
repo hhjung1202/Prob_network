111bd4cb4dd22c7cbcbb3fac4ab255be9303dc6e
unknown <hhjung1202@naver.com>
2018-09-21 04:37:29 +0900

    pruning

---
+++ 20180921/Proposed_gpu1.py
@@ -0,0 +1,214 @@
+import torch
+from torch.autograd import Variable
+import torch.optim as optim
+from torchvision import datasets, transforms
+from Pmodel import *
+import os
+import torch.backends.cudnn as cudnn
+import time
+import utils
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+
+def main(model_dir, model, dataset):
+    utils.default_model_dir = model_dir
+    utils.c = None
+    utils.str_w = ''
+    # model = model
+    lr = 0.1
+    start_time = time.time()
+
+    if dataset == 'cifar10':
+        train_loader, test_loader = utils.cifar10_loader()
+    elif dataset == 'cifar100':
+        train_loader, test_loader = utils.cifar100_loader()
+
+    if torch.cuda.is_available():
+        # os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+        print("USE", torch.cuda.device_count(), "GPUs!")
+        model = nn.DataParallel(model).cuda()
+        cudnn.benchmark = True
+
+    else:
+        print("NO GPU -_-;")
+
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    start_epoch = 0
+    checkpoint = utils.load_checkpoint(model_dir)
+    
+    if not checkpoint:
+        pass
+    else:
+        start_epoch = checkpoint['epoch'] + 1
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+
+    utils.init_learning(model.module)
+
+    for epoch in range(start_epoch, 165):
+        if epoch < 80:
+            learning_rate = lr
+        elif epoch < 120:
+            learning_rate = lr * 0.1
+        else:
+            learning_rate = lr * 0.01
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = learning_rate
+
+        train(model, optimizer, criterion, train_loader, epoch, True)
+        test(model, criterion, test_loader, epoch, True)
+
+        utils.switching_learning(model.module)
+        print('switching_learning to Gate')
+        
+        train(model, optimizer, criterion, train_loader, epoch, False)
+        test(model, criterion, test_loader, epoch, False)        
+
+        utils.switching_learning(model.module)
+        print('switching_learning to Gate')
+
+        if epoch % 5 == 0:
+            model_filename = 'checkpoint_%03d.pth.tar' % epoch
+            utils.save_checkpoint({
+                'epoch': epoch,
+                'model': model,
+                'state_dict': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+            }, model_filename, model_dir)
+
+    now = time.gmtime(time.time() - start_time)
+    weight_extract(model, optimizer, criterion, train_loader, epoch)
+    utils.conv_weight_L1_printing(model.module)
+    
+    print('{} hours {} mins {} secs for training'.format(now.tm_hour, now.tm_min, now.tm_sec))
+    
+
+def train(model, optimizer, criterion, train_loader, epoch, is_main):
+    model.train()
+    train_loss = 0
+    total = 0
+    correct = 0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if torch.cuda.is_available():
+            data, target = Variable(data.cuda()), Variable(target.cuda())
+        else:
+            data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.data[0]
+        _, predicted = torch.max(output.data, 1)
+        total += target.size(0)
+        correct += predicted.eq(target.data).cpu().sum()
+        if batch_idx % 10 == 0 and is_main is True:
+            utils.print_log('Epoch: {} | Batch: {} |  Loss: ({:.4f}) | Acc: ({:.2f}%) ({}/{})'
+                  .format(epoch, batch_idx, train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+            print('Epoch: {} | Batch: {} |  Loss: ({:.4f}) | Acc: ({:.2f}%) ({}/{})'
+                  .format(epoch, batch_idx, train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+        elif batch_idx % 10 == 0 and is_main is False:
+            utils.print_log('SWICH: {} | Batch: {} |  Loss: ({:.4f}) | Acc: ({:.2f}%) ({}/{})'
+                  .format(epoch, batch_idx, train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+            print('SWICH: {} | Batch: {} |  Loss: ({:.4f}) | Acc: ({:.2f}%) ({}/{})'
+                  .format(epoch, batch_idx, train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+
+def weight_extract(model, optimizer, criterion, train_loader, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if torch.cuda.is_available():
+            data, target = Variable(data.cuda()), Variable(target.cuda())
+        else:
+            data, target = Variable(data), Variable(target)
+        
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+
+        utils.c = target.view(-1,1) # batch array torch.tensor[128]
+        utils.c = utils.c.type(torch.cuda.FloatTensor)
+        utils.weight_extract(model.module)
+
+        for i in utils.c:
+            for j in i:
+                utils.str_w = utils.str_w + str(j.tolist()) + ','
+            utils.str_w += '\n'
+
+        utils.save_to_csv()
+        utils.str_w = ''
+
+        if batch_idx % 100 == 0:
+            print('Epoch: {}'.format(epoch))
+
+
+def test(model, criterion, test_loader, epoch, is_main):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (data, target) in enumerate(test_loader):
+        if torch.cuda.is_available():
+            data, target = Variable(data.cuda()), Variable(target.cuda())
+        else:
+            data, target = Variable(data), Variable(target)
+
+        outputs = model(data)
+        loss = criterion(outputs, target)
+
+        test_loss += loss.data[0]
+        _, predicted = torch.max(outputs.data, 1)
+        total += target.size(0)
+        correct += predicted.eq(target.data).cpu().sum()
+
+    max_result.append(correct)
+    if is_main is True:
+        utils.print_log('# TEST : Epoch : {} | Loss: ({:.4f}) | Acc: ({:.2f}%) ({}/{}) | Err: ({:.2f}%) | Max: ({})'
+          .format(epoch, test_loss/(batch_idx+1), 100.*correct/total, correct, total, 100-100.*correct/total, max(max_result)))
+        print('# TEST : Epoch : {} | Loss: ({:.4f}) | Acc: ({:.2f}%) ({}/{}) | Err: ({:.2f}% | Max: ({}))'
+          .format(epoch, test_loss/(batch_idx+1), 100.*correct/total, correct, total, 100-100.*correct/total, max(max_result)))
+
+    elif is_main is False:
+        utils.print_log('$ TEST_S : Epoch : {} | Loss: ({:.4f}) | Acc: ({:.2f}%) ({}/{}) | Err: ({:.2f}%) | Max: ({})'
+          .format(epoch, test_loss/(batch_idx+1), 100.*correct/total, correct, total, 100-100.*correct/total, max(max_result)))
+        print('$ TEST_S : Epoch : {} | Loss: ({:.4f}) | Acc: ({:.2f}%) ({}/{}) | Err: ({:.2f}% | Max: ({}))'
+          .format(epoch, test_loss/(batch_idx+1), 100.*correct/total, correct, total, 100-100.*correct/total, max(max_result)))
+
+layer_set = [14, 20, 32, 44, 56, 110]
+
+if __name__=='__main__':
+    
+    max_result = []
+    model_dir = '../hhjung/Proposed/cifar10/Resnet14'
+    model_selection = ResNet(num_gate=7,num_classes=10,resnet_layer=layer_set[0])
+    dataset = 'cifar10'
+    main(model_dir, model_selection, dataset)
+    
+    max_result = []
+    model_dir = '../hhjung/Proposed/cifar10/Resnet20'
+    model_selection = ResNet(num_gate=7,num_classes=10,resnet_layer=layer_set[1])
+    dataset = 'cifar10'
+    main(model_dir, model_selection, dataset)
+
+    max_result = []
+    model_dir = '../hhjung/Proposed/cifar10/Resnet32'
+    model_selection = ResNet(num_gate=7,num_classes=10,resnet_layer=layer_set[2])
+    dataset = 'cifar10'
+    main(model_dir, model_selection, dataset)
+
+    max_result = []
+    model_dir = '../hhjung/Proposed/cifar10/Resnet44'
+    model_selection = ResNet(num_gate=7,num_classes=10,resnet_layer=layer_set[3])
+    dataset = 'cifar10'
+    main(model_dir, model_selection, dataset)
+
+    max_result = []
+    model_dir = '../hhjung/Proposed/cifar10/Resnet56'
+    model_selection = ResNet(num_gate=7,num_classes=10,resnet_layer=layer_set[4])
+    dataset = 'cifar10'
+    main(model_dir, model_selection, dataset)
+
---
+++ 20180921/utils.py
@@ -0,0 +1,319 @@
+import torch
+import torch.nn as nn
+from torchvision import datasets, transforms
+import csv
+import os
+from operator import add
+
+default_model_dir = "./"
+c = None
+str_w = ''
+
+def get_num_gen(gen):
+    return sum(1 for x in gen)
+
+def is_leaf(model):
+    return get_num_gen(model.children()) == 0
+
+def init_learning(model):
+    for child in model.children():
+        if hasattr(child, 'phase'):
+            turn_off_learning(child)
+        elif is_leaf(child):
+            if hasattr(child, 'weight'):
+                child.weight.requires_grad = True
+                # print('True', child)
+        else:
+            init_learning(child)
+
+def init_learning_phase4(model):
+    for child in model.children():
+        if is_leaf(child):
+            if hasattr(child, 'weight'):
+                child.weight.requires_grad = True
+                # print('True', child)
+        else:
+            init_learning_phase4(child)
+
+def turn_off_learning(model):
+    if is_leaf(model):
+        if hasattr(model, 'weight'):
+            model.weight.requires_grad = False
+            # print('False', model)
+        return
+
+    for child in model.children():
+        if is_leaf(child):
+            if hasattr(child, 'weight'):
+                child.weight.requires_grad = False
+                # print('False', child)
+        else:
+            turn_off_learning(child)
+
+
+def switching_learning(model):
+    if is_leaf(model):
+        if hasattr(model, 'weight'):
+            if model.weight.requires_grad:
+                model.weight.requires_grad = False
+                # print('False', model)
+            else:
+                model.weight.requires_grad = True
+                # print('True', model)
+        return
+    
+    for child in model.children():
+
+        if is_leaf(child):
+            if hasattr(child, 'weight'):
+                if child.weight.requires_grad:
+                    child.weight.requires_grad = False
+                    # print('False', child)
+                else:
+                    child.weight.requires_grad = True
+                    # print('True', child)
+        else:
+            switching_learning(child)
+
+
+
+
+class is_on(object):
+    def __init__(self):
+        self.is_phase2 = False
+        self.is_test = False
+        self.is_phase4 = False
+
+    def check_is_phase2(self):
+        return self.is_phase2
+
+    def change_phase2(self):
+        if self.is_phase2:
+            self.is_phase2 = False
+        else:
+            self.is_phase2 = True
+
+    def change_on_phase2(self):
+        if not self.is_phase2:
+            self.is_phase2 = True
+
+    def check_is_phase4(self):
+        return self.is_phase4
+
+    def change_on_phase4(self):
+        if not self.is_phase4:
+            self.is_phase4 = True
+
+    def check_is_test(self):
+        return self.is_test
+
+    def change_test(self):
+        if self.is_test:
+            self.is_test = False
+        else:
+            self.is_test = True
+
+def save_checkpoint(state, filename, model_dir):
+
+    # model_dir = 'drive/app/torch/save_Routing_Gate_2'
+    model_filename = os.path.join(model_dir, filename)
+    latest_filename = os.path.join(model_dir, 'latest.txt')
+
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+
+    with open(latest_filename, 'w') as fout:
+        fout.write(model_filename)
+
+    torch.save(state, model_filename)
+    print("=> saving checkpoint '{}'".format(model_filename))
+
+    return
+
+def load_checkpoint(model_dir):
+
+    # model_dir = 'drive/app/torch/save_Routing_Gate_2'
+    latest_filename = os.path.join(model_dir, 'latest.txt')
+    if os.path.exists(latest_filename):
+        with open(latest_filename, 'r') as fin:
+            model_filename = fin.readlines()[0]
+    else:
+        return None
+    print("=> loading checkpoint '{}'".format(model_filename))
+    state = torch.load(model_filename)
+
+    return state
+
+def routing_weight_printing(model):
+    for child in model.children():
+        if hasattr(child, 'phase'):
+            print_log('percent {} {}'.format(str(child.p.data), str(child.p.data.size())))
+            print('percent', child.p.data, child.p.data.size())
+        elif is_leaf(child):
+            continue
+        else:
+            routing_weight_printing(child)
+
+def conv_weight_L1_printing(model):
+    for child in model.children():
+        if is_leaf(child):
+            if isinstance(child, nn.Conv2d):
+                print_log('{} {} {}'.format(str(child) ,str(child.weight.size()), str(child.weight.norm(1))))
+                print(child, child.weight.size(), child.weight.norm(1))
+        else:
+            conv_weight_L1_printing(child)
+
+def conv_weight_L2_printing(model):
+    for child in model.children():
+        if is_leaf(child):
+            if isinstance(child, nn.Conv2d):
+                print_log('{} {} {}'.format(str(child) ,str(child.weight.size()), str(child.weight.norm(2))))
+                print(child, child.weight.size(), child.weight.norm(2))
+        else:
+            conv_weight_L1_printing(child)
+
+
+def print_log(text, filename="log.txt"):
+    if not os.path.exists(default_model_dir):
+        os.makedirs(default_model_dir)
+    model_filename = os.path.join(default_model_dir, filename)
+    with open(model_filename, "a") as myfile:
+        myfile.write(text + "\n")
+
+def weight_extract(model):
+    global c
+    if c is not None:
+        for child in model.children():
+            if hasattr(child, 'phase'):
+                c = torch.cat([c, child.p.view(-1,1)], 1)
+            elif is_leaf(child):
+                continue
+            else:
+                weight_extract(child)
+
+
+def save_to_csv():
+    global str_w
+    if not os.path.exists(default_model_dir):
+        os.makedirs(default_model_dir)
+
+    model_filename = os.path.join(default_model_dir, 'weight.csv')
+    with open(model_filename, 'a') as fout:
+        fout.write(str_w)
+
+def load_gate_csv():
+    class_counter = {} # {0: 5000, 1: 5000, ...}
+    class_weight_sum = {} # {0: [, , , , , ], 1: [, , , , , ], ...}
+
+    model_filename = os.path.join(default_model_dir, 'weight.csv')
+    with open(model_filename, newline="") as csvfile:
+            spam = csv.reader(csvfile)
+            for row in spam:
+                row = [float(i) for i in row if len(i) > 0]
+
+                if row[0] in class_counter:
+                    print(row[1:])
+                    # row[0] is class
+                    class_counter[row[0]] = class_counter[row[0]] + 1
+                    class_weight_sum[row[0]] = class_weight_sum[row[0]] + torch.tensor(row[1:])
+                else:
+                    print(row[1:])
+                    class_counter[row[0]] = 1
+                    class_weight_sum[row[0]] = torch.tensor(row[1:], dtype=torch.float)
+    
+    class_average, total_average = get_averages(class_counter, class_weight_sum)
+    return class_counter, class_weight_sum, class_average, total_average
+
+def get_averages(class_counter, class_weight_sum):
+    class_average = {}
+    total_average = None
+    total_num = 0
+    for i in class_counter:
+
+        class_average[i] = class_weight_sum[i] / class_counter[i]
+        if total_average is None:
+            total_average = class_weight_sum[i]
+        else:
+            total_average = total_average + class_weight_sum[i]
+        total_num += class_counter[i]
+    total_average = total_average / total_num
+
+    return class_average, total_average
+
+
+
+def cifar10_loader():
+    batch_size = 128
+    print("cifar10 Data Loading ...")
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.4914, 0.4824, 0.4467),
+                             std=(0.2471, 0.2436, 0.2616))
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.4914, 0.4824, 0.4467),
+                             std=(0.2471, 0.2436, 0.2616))
+    ])
+
+    train_dataset = datasets.CIFAR10(root='../hhjung/cifar10/',
+                                     train=True,
+                                     transform=transform_train,
+                                     download=True)
+
+    test_dataset = datasets.CIFAR10(root='../hhjung/cifar10/',
+                                    train=False,
+                                    transform=transform_test)
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               num_workers=4)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=batch_size,
+                                              shuffle=False,
+                                              num_workers=4)
+    return train_loader, test_loader
+
+
+def cifar100_loader():
+    batch_size = 128
+    print("cifar100 Data Loading ...")
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.5071, 0.4867, 0.4408),
+                             std=(0.2675, 0.2565, 0.2761))
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.5071, 0.4867, 0.4408),
+                             std=(0.2675, 0.2565, 0.2761))
+    ])
+
+    train_dataset = datasets.CIFAR100(root='../hhjung/cifar100/',
+                                     train=True,
+                                     transform=transform_train,
+                                     download=True)
+
+    test_dataset = datasets.CIFAR100(root='../hhjung/cifar100/',
+                                    train=False,
+                                    transform=transform_test)
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               num_workers=4)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=batch_size,
+                                              shuffle=False,
+                                              num_workers=4)
+
+    return train_loader, test_loader
